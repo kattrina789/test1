12c62f4e8a661529708b6c0cdd2535aa1a2861b5
kattrina <kattrinayt@gmail.com>
2024-04-29 22:31:38 +0300

    д/з строки

---
+++ sharp_test/Program.cs
@@ -0,0 +1,21 @@
+﻿string text = "gjhgggjhgy uuyiu iuyiuy";
+string[] words = text.Split(new char[] { ' ' });
+
+void PrintArray(string[] arr)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Thread.Sleep(1000);
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1)
+            System.Console.Write(", ");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    System.Console.WriteLine("]");
+    Console.ResetColor();
+}
+
+PrintArray(words);
